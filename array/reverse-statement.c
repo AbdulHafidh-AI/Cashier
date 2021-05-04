e3d4b135f2bbef0ca7247c7911a5792a5605c267
Bravehard700 <xquatorlegion@gmail.com>
2021-05-05 06:19:12 +0700

    Create reverse-statement.c

---
+++ array/reverse-statement.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() 
+{
+    // Inisialisasi Variabel
+    char kalimat[100];
+    // User Input Kalimat
+    printf("Masukan sebuah kalimat: ");
+    scanf("%[^\n]s",kalimat);
+    // Cari panjang array yang keiisi
+    int panjang = 0;
+    while (kalimat[panjang] != '\0') 
+    {
+        panjang++;
+    }
+    printf("%d", panjang);
+    
+    for (int i = panjang; i >= 0 ;i--)
+    {
+        printf("%c",kalimat[i]);
+    }
+    
+    return 0;
+}
